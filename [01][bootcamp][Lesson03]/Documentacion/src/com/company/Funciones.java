64f0afafdc344e77066b621524c74896da444ec4
ronaldmelara <ronaldmelara@gmail.com>
2022-09-22 12:41:41 -0300

    Manejo de funciones

---
+++ [01][bootcamp][Lesson03]/Documentacion/src/com/company/Funciones.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Funciones {
+    public static void main(String[] args) {
+
+        HolaMundo();
+        System.out.println(devolverHolaMundo());
+        HolaMundo("Ronald");
+    }
+
+    private static void HolaMundo() {
+        System.out.println("Hola Mundo desde un metodo");
+    }
+
+    private static void HolaMundo(String nombre) {
+        System.out.println("Hola Mundo desde un metodo " + nombre);
+    }
+    private static String devolverHolaMundo(){
+        return "Hola mundo desde un metodo con retorno";
+    }
+
+
+}
