7750b47d64c43e8d286127611f7a884e7fe13ca5
ronaldmelara <ronaldmelara@gmail.com>
2022-09-22 11:35:28 -0300

    Manejo de Tipos de Datos en Java

---
+++ [01][bootcamp][Lesson03]/Documentacion/src/com/company/Tipos.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Tipos {
+    public static void main(String[] args) {
+
+        //tipo identificador = <<valor>>;
+        //otra forma es:
+        //tipo identificador;
+        //identificador = <<valor>>;
+
+        //Enteros
+        byte number1 = 1; //1 byte
+        short number2 = 2; //2 byte
+        int number3 = 3; //3 byte
+        long number4 = 4; //4 byte
+
+        //punto flotante
+        float numero1 = 4.9f;
+        double numero2 = 4.9d;
+
+        //caracter
+        char caracter1 = 'a';
+
+        //boleanos
+        boolean verdadero = true;
+        boolean falso = false;
+
+        //cadena de textos
+        String nombre = "Ronald";
+        String apellido = "Melara";
+
+        //tipos envoltorios: permite manejar nulos
+        Integer numero3 = null;
+        Long numero4 = 2L;
+    }
+}
