31787046d5d01f56accbc3e3b72e66e2b147def1
ronaldmelara <ronaldmelara@gmail.com>
2022-09-23 15:40:11 -0300

    Reorganizacion de Repositorio

---
+++ Udemy/02-utilizar-apache-netbean/ejemplo_netbeans/src/main/java/aeropuerto/Activable.java
@@ -0,0 +1,15 @@
+package aeropuerto;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author melaragalaz
+ */
+public interface Activable {
+    public boolean isActivado();
+    public void setActivado(boolean value);
+}
