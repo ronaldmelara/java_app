777f0f4a0d15138906efa671bcc0708796e05333
ronaldmelara <ronaldmelara@gmail.com>
2022-12-21 15:52:07 -0300

    Mi primer proyecto con Spring Data JPA

---
+++ Lecciones_BootCamp/Avanzado/ob-springdatajpa/src/main/java/com/example/obspringdatajpa/ObSpringdatajpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.obspringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObSpringdatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObSpringdatajpaApplication.class, args);
+	}
+
+}
